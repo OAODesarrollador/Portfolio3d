da487f20097b86f6db6f3ac7d99fd7e9298854ca
oaodesarrollador <oaodesarrollador@gmail.com>
2025-01-24 14:24:34 -0300

    Modifico Seccion scroll H

--- src/componentes/HorizontalScroll/Seccion.tsx
+++ src/componentes/HorizontalScroll/Seccion.tsx
@@ -13,34 +13,28 @@ export const Section: React.FC<SectionProps> = ({ title, content, color }) => {
   <Container>
     <Row className="justify-content-center" style={{ height: '100vh', scrollSnapAlign: 'start', width: '100vw' }}>
       <Col xs={12} md={6} className="home-content" style={{ height: '100vh', width: '40vw', margin: '0%' }}></Col> 
-      <Col xs={12} md={6}>
-    <motion.div
-      className="seccion"
-      style={{
-        backgroundColor: color,
-        width: '40vw',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexShrink: 0
-      }}
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 6}}
-    >
-    
-        
-          
+      <Col xs={12} md={6} >
+        <motion.div
+          className="seccion"
+          style={{
+            backgroundColor: color,
+            width: '40vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1}}
+        >
           <h2>{title}</h2>
           <div>
             <p>{content}</p>
           </div>
-          
-          
-      
-    </motion.div></Col>
-    
+        </motion.div>
+      </Col>  
     </Row>
     </Container>
   )
